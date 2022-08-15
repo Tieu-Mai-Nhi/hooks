76f8dfdc08ea09816af4fca6eb99338fcc2982d8
Mai Duc Thang <thangmdfx12498@funix.edu.vn>
2022-08-15 17:29:50 +0700

    Add todoList

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
+import { useState } from 'react';
 import './App.scss';
-import ColorBox from './components/ColorBox';
+import TodoList from './components/TodoList'
 
 function App() {
+  
+  const [todoList, setTodoList] = useState([
+    { id: '1', title: 'Eat' },
+    { id: '2', title: 'Sleep' },
+    { id: '3', title: 'Code'}
+  ])
+  
+  function handleTodoClick(todo) {
+    // lấy được todo được click rồi
+    console.log(todo);
+    // tìm vị trí
+    const index = todoList.findIndex(x => x.id === todo.id);
+    if (index == -1) return;
+
+    const newTodoList = [...todoList] //clone nếu muốn dùng mảng đó
+    newTodoList.splice(index, 1);
+    setTodoList(newTodoList);
+    // 
+  }
+  
+  
+
   return (
     <div className="app">
       <h1>Hooks</h1>
-      <ColorBox />
+      <TodoList todos={todoList} onTodoClick={handleTodoClick}/>
     </div>
   );
 }
